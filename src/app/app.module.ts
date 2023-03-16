5bd0a36a54780bee37c534e69f94c23d95c4bb3c
Estefania Fajardo <a.fajardor@uniandes.edu.co>
2023-03-16 06:42:00 -0500

    "Log in"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GrupoComponent } from './grupo/grupo.component';
-import { GrupoListaComponent } from './grupo-lista/grupo-lista.component';
-import { GrupoCreateComponent } from './grupo-create/grupo-create.component';
+import { GrupoListaComponent } from './grupo/grupo-lista/grupo-lista.component';
+import { GrupoCreateComponent } from './grupo/grupo-create/grupo-create.component';
 import { AlarmaComponent } from './alarma/alarma.component';
-import { AlarmaListaComponent } from './alarma-lista/alarma-lista.component';
-import { AlarmaCreateComponent } from './alarma-create/alarma-create.component';
+import { AlarmaListaComponent } from './alarma/alarma-lista/alarma-lista.component';
+import { AlarmaCreateComponent } from './alarma/alarma-create/alarma-create.component';
+
 
 @NgModule({
   declarations: [
